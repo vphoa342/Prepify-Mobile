5ca8a62f1203b6feb9e351b69b43890fd42f8d36
vphoa342 <vphoa34@gmail.com>
2024-05-22 22:01:49 +0700

    first commit

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,17 @@
+import { View } from "react-native";
+import Member from "./components/Member";
+import React from "react";
+
+const About = () => {
+  return (
+    <View>
+      <Member />
+      <Member />
+      <Member />
+      <Member />
+      <Member />
+    </View>
+  );
+};
+
+export default About;
---
+++ src/pages/About/components/Member/Member.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const Member = () => {
+  return <View>Member</View>;
+};
+
+export default Member;
