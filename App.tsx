2efb7d2b052f382b8b9e4b87587e098338584562
vphoa34 <vphoa34@gmail.com>
2024-05-31 16:19:50 +0700

    Merge complete

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { AuthProvider } from "src/contexts/auth/AuthContext";
 import { AppNavigation } from "src/navigation/AppNavigation";
 
+
 export default function App() {
-    return (
+return (
         <AuthProvider>
             <AppNavigation />
         </AuthProvider>
     );
+
 }
